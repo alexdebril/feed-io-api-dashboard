5e9b98f6e1273b1ba93025fa1f6edd8ff9b77ec8
Alex Debril <alexdebril@users.noreply.github.com>
2021-02-18 22:43:58 +0100

    Init the list

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Item } from '../item';
 
 @Component({
   selector: 'app-item',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item.component.css']
 })
 export class ItemComponent implements OnInit {
+
+  items: Item[] = [];
 
   constructor() { }
 
