b434aad6d4ef9679a603c4930c981dd4727a203a
Alex Debril <alexdebril@users.noreply.github.com>
2021-02-24 22:29:55 +0100

    API URLs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   urlWebsocket: '//api.feed-io.net/ws',
+  urlApi: '//api.feed-io.net',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,7 @@
 export const environment = {
   production: false,
   urlWebsocket: '//ws.localhost',
+  urlApi: '//api.localhost',
 };
 
 /*
